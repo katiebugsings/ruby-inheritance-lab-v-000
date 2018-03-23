ea96d05e29427f8c1444c302847c49852af10154
Katie Cassell <kcassell944@gmail.com>
2018-03-23 22:02:48 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,18 +1,16 @@
 require 'pry'
 class Teacher < User
-attr_accessor :first_name, :last_name
+#attr_accessor :first_name, :last_name
 
-def initialize
-  @first_name = first_name
-  @last_name = last_name
-end
+#def initialize(first_name, last_name)
+  #@first_name = first_name
+  #@last_name = last_name
+#end
 
 def teach
   self.teach
   knowledge = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 end
 
-
-binding.pry
 
 end
