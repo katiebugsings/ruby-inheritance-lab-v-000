99f6aba174fa4dd8890914ea7bc33fea1dea6dfa
Katie Cassell <kcassell944@gmail.com>
2018-03-23 22:03:46 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class Teacher < User
-#attr_accessor :first_name, :last_name
+attr_accessor :first_name, :last_name
 
-#def initialize(first_name, last_name)
-  #@first_name = first_name
-  #@last_name = last_name
-#end
+def initialize(first_name, last_name)
+  @first_name = first_name
+  @last_name = last_name
+end
 
 def teach
   self.teach
