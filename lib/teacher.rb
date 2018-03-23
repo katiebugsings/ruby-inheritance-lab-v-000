e958cff73238728329d57a123de07882e144b4a4
Katie Cassell <kcassell944@gmail.com>
2018-03-23 21:49:53 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -8,7 +8,7 @@ end
 
 def teach
   knowledge = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-end  
+end
 
 
 
